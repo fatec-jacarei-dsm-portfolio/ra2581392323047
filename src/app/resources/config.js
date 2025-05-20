70331b15503f42862b23a76fbf1103d0dee81a59
itsmorais <moraisdpm@outlook.com>
2025-05-20 12:14:31 -0300

    solved infinity spinner loading on about

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -5,15 +5,17 @@ const i18n = true;
 
 // Manage localized content in the messages folder
 const i18nOptions = {
-    locales: ['en','pt'],            // A list of all locales that are supported, e.g. ['en','id']
+    locales: ['en', 'pt'],            // A list of all locales that are supported, e.g. ['en','id']
     defaultLocale: 'pt'         // Locale used by default and as a fallback
 }
 
 const routes = {
-    '/':        true,
-    '/about':   true,
-    '/work':    true,
-    '/blog':    true,
+    '/': true,
+    '/about': true,
+    "/pt/about": true,
+    "/en/about": true,
+    '/work': true,
+    '/blog': true,
     '/gallery': true,
 }
 
@@ -40,20 +42,20 @@ const effects = {
 }
 
 const style = {
-    theme:       'dark',         // dark | light
-    neutral:     'gray',         // sand | gray | slate
-    brand:       'emerald',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    accent:      'indigo',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-    solid:       'contrast',     // color | contrast
-    solidStyle:  'flat',         // flat | plastic
-    border:      'playful',      // rounded | playful | conservative
-    surface:     'translucent',  // filled | translucent
-    transition:  'all'           // all | micro | macro
+    theme: 'dark',         // dark | light
+    neutral: 'gray',         // sand | gray | slate
+    brand: 'emerald',      // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    accent: 'indigo',       // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+    solid: 'contrast',     // color | contrast
+    solidStyle: 'flat',         // flat | plastic
+    border: 'playful',      // rounded | playful | conservative
+    surface: 'translucent',  // filled | translucent
+    transition: 'all'           // all | micro | macro
 }
 
 const display = {
     location: true,
-    time:     true
+    time: true
 }
 
 
